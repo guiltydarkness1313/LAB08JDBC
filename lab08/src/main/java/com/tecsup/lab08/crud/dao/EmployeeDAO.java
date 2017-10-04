3e7371a1638bfa8348900bda3a27816f5e4bab6d
paulf <paulf@10.200.169.198>
2017-10-04 11:52:53 -0500

    initial commit

---
+++ lab08/src/main/java/com/tecsup/lab08/crud/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package com.tecsup.lab08.crud.dao;
+
+import java.util.ArrayList;
+
+import com.tecsup.lab08.crud.bean.Employee;
+
+public interface EmployeeDAO {
+	
+	ArrayList<Employee> findAll();
+	
+	Employee findById(int id);
+	
+	
+	int create(String pFirstname, String pLastname, Double pSalary);
+	
+	void delete(int id);
+	
+	void update(int id, String pFirstname, String pLastname, Double pSalary);
+}
